7b77fb05047ffc83cad38e91f9dfb09892b9aadc
wapastorv <williampastor@outlook.com>
2024-02-11 20:18:40 -0500

    Variables y Operadores

---
+++ operadores.py
@@ -0,0 +1,28 @@
+# Operadores #
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(21 % 2)
+
+print(21 // 2)
+
+
+
+
+# operadores comparativos mas comunes#
+print("operadores de comparacion mas utilizados")
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Operadores Logicos #
+print("operadores de logicos")
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,14 @@
+## Variables
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+
+my_int_variable =  5
+print(my_int_variable)
+
+my_int_to_string_variable = str(my_int_variable)
+print(my_int_to_string_variable)
+print(type(my_int_to_string_variable))
+
+print(len(my_string_variable))
+
